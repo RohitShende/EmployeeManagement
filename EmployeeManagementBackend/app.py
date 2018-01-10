ca025b07037769f1edaac587ba281b07dfeb2011
Rohit Shende <rohitshende16@gmail.com>
2018-01-11 01:54:57 +0530

    Creating the backend project structure

---
+++ EmployeeManagementBackend/app.py
@@ -0,0 +1,12 @@
+ Volume in drive E is Sonu
+ Volume Serial Number is 1AB8-CFB3
+
+ Directory of E:\Studies\Rohit\PROJECTS\EmployeeManagement\EmployeeManagementBackend
+
+01/11/2018  01:50 AM    <DIR>          .
+01/11/2018  01:50 AM    <DIR>          ..
+01/11/2018  01:49 AM    <DIR>          .idea
+01/11/2018  01:50 AM                 0 app.py
+01/11/2018  01:17 AM                 0 hello-world.py
+               2 File(s)              0 bytes
+               3 Dir(s)  48,032,710,656 bytes free
